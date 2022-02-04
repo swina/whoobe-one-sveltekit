3f3a32b58835bfecd304b749d3db8108d54146bc
Antonio Nardone <antnardo@publicisgroupe.net>
2022-02-04 12:05:59 +0300

    Added 404.html as fallback in config

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
 import adapter from '@sveltejs/adapter-static';
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({
+			fallback: '404.html'
+		}),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 	}
